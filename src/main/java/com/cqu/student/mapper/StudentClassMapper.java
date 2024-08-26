4c9933ab273c13d62f9a7722dc29b2f874ca0f79
哈哈哈 <2238882309@qq.com>
2024-08-26 22:47:53 +0800

    选课接口更新

--- src/main/java/com/cqu/student/mapper/StudentClassMapper.java
+++ src/main/java/com/cqu/student/mapper/StudentClassMapper.java
@@ -4,9 +4,11 @@ package com.cqu.student.mapper;
 import com.cqu.student.pojo.StudentClass;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface StudentClassMapper {
     int classSuccess(int stuId, int classId);
 
-    StudentClass ifChosen(int stuId, int classId);
+    List<Integer> ifChosen(Integer stuId);
 }
--- src/main/java/com/cqu/student/service/StudentClassService.java
+++ src/main/java/com/cqu/student/service/StudentClassService.java
@@ -2,7 +2,9 @@ package com.cqu.student.service;
 
 import com.cqu.student.pojo.StudentClass;
 
+import java.util.List;
+
 public interface StudentClassService {
     public int classIfSuccess(int stuId, int classId);
-    public StudentClass ifChosen(int stuId, int classId);
+    List<Integer> ifChosen(Integer stuId);
 }
